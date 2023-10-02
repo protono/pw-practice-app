3d09d402a2c993c602623f2594151e408fd77000
Sylvain Pelissier <sylvain.pelissier@grandvision.com>
2023-10-02 15:23:31 +0200

    remove duplication using a hook

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,13 +1,14 @@
 import { test } from '@playwright/test'
+test.beforeEach(async ({ page }) => {
+    await page.goto('')
+    await page.getByText('Forms').click()
+})
+
 test.describe('first suite', () => {
     test('first test', async ({ page }) => {
-        await page.goto('')
-        await page.getByText('Forms').click()
         await page.getByText('Form layouts').click()
     })
     test('open datepicker layouts page', async ({ page }) => {
-        await page.goto('')
-        await page.getByText('Forms').click()
         await page.getByText('Datepicker').click()
     })
 })
