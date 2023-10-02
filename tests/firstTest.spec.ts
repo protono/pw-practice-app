e3d022bd3100780b9c9769cfafb247cde5c32d2a
Sylvain Pelissier <sylvain.pelissier@grandvision.com>
2023-10-02 18:18:34 +0200

    reusing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 test.beforeEach(async ({ page }) => {
     await page.goto('')
 })
@@ -19,41 +19,53 @@ test.describe('locators', () => {
         await page.getByText('Form layouts').click()
     })
     test('base locators syntax rules', async ({ page }) => {
-        await page.locator('input').first().highlight() // tag
-        await page.locator('.shape-rectangle').first().highlight() // class
-        await page.locator('#inputEmail').first().highlight() // id
-        await page.locator('[placeholder="Email"]').first().highlight() // attribute
-        await page.locator('input[placeholder="Email"].shape-rectangle').first().highlight() // combination
-        await page.locator(':text-is("Email")').first().highlight() // css
+        await page.locator('input').first().focus() // tag
+        await page.locator('.shape-rectangle').first().focus() // class
+        await page.locator('#inputEmail').first().focus() // id
+        await page.locator('[placeholder="Email"]').first().focus() // attribute
+        await page.locator('input[placeholder="Email"].shape-rectangle').first().focus() // combination
+        await page.locator(':text-is("Email")').first().focus() // css
     })
     test('user facing locators', async ({ page }) => {
-        await page.getByRole('textbox').first().highlight()
-        await page.getByRole('button').first().highlight()
-        await page.getByLabel('password').first().highlight()
-        await page.getByPlaceholder('email').first().highlight()
-        await page.getByText('name').first().highlight()
-        await page.getByTitle('IoT Dasshboard').first().highlight()
-        await page.getByTestId('test-name').first().highlight()
+        await page.getByRole('textbox').first().focus()
+        await page.getByRole('button').first().focus()
+        await page.getByLabel('password').first().focus()
+        await page.getByPlaceholder('email').first().focus()
+        await page.getByText('name').first().focus()
+        await page.getByTitle('IoT Dashboard').first().focus()
+        await page.getByTestId('test-name').first().focus()
     })
     test('child elements', async ({ page }) => {
-        await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 1")').first().highlight()
+        await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 1")').first().focus()
         // equivalent, just shorter
-        await page.locator('nb-card nb-radio :text-is("Option 2")').first().highlight()
+        await page.locator('nb-card nb-radio :text-is("Option 2")').first().focus()
         // mixing locator types
-        await page.locator('nb-card').getByRole('button', { name: 'Sign In' }).first().highlight()
+        await page.locator('nb-card').getByRole('button', { name: 'Sign In' }).first().focus()
     })
-    test.only('parent elements', async ({ page }) => {
+    test('parent elements', async ({ page }) => {
         const card = page.locator('nb-card')
         const child = page.getByPlaceholder('Message')
         const parent = card.filter({ has: child })
 
-        await parent.highlight()
+        await parent.focus()
 
         await card
             .filter({ hasText: 'Form without labels' })
             .getByRole('textbox', { name: 'Message' })
-            .highlight()
+            .focus()
 
-        await child.locator('../../../..').highlight()
+        await child.locator('../../../..').focus()
+    })
+    test('reusing locators', async ({ page }) => {
+        let card = page.locator('nb-card')
+        card = card.filter({ hasText: 'Basic form' })
+        const password = card.getByPlaceholder('Password')
+
+        await card.getByPlaceholder('Email').fill('test@test.com')
+        await password.fill('xxx')
+        await expect(password).toHaveValue('xxx')
+        await card.getByRole('checkbox').check({ force: true })
+        await card.getByRole('button').focus()
+        await expect(card.getByRole('button')).toBeFocused()
     })
 })
