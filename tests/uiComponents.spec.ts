8945b3f8d4d72c47e9cb2a451b7428cd66439c52
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-03 18:12:27 +0200

    ui: web tables

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -95,6 +95,44 @@ test.describe('UI components', () => {
             await row.locator('.nb-trash').click()
             await expect(row).not.toContainText('@mdo')
         })
-    })
+        test('web tables', async ({ page }) => {
+            const card = page.locator('nb-card').filter({ hasText: 'Smart Table' })
+            const table = card.locator('tbody')
+            var row = table.getByRole('row', { name: '@twitter' })
+            await row.locator('.nb-edit').click()
+            var cell = table.getByPlaceholder('Age')
+            await cell.clear()
+            await cell.fill('36')
+            await table.locator('.nb-checkmark').click()
+            await expect(row).toContainText('36')
 
+            const pager = card.locator('ng2-smart-table-pager')
+            const pages = pager.getByRole('list').getByRole('listitem')
+            await pages.getByText('3').click()
+            // intersection between two datasets (but use 'page.' in the has: clause)
+            row = table.getByRole('row', { name: '27' }).filter({ has: page.locator('td').nth(1).getByText('27') })
+            await row.locator('.nb-edit').click()
+            await cell.clear()
+            await cell.fill('27')
+            await table.locator('.nb-checkmark').click()
+            await expect(row.locator('td').nth(6)).toContainText('27')
+
+            await pages.getByText('First').click()
+            const ages = ['20', '30', '40', '50']
+            for (let age of ages) {
+                await card.locator('input-filter').getByPlaceholder('Age').clear()
+                await card.locator('input-filter').getByPlaceholder('Age').fill(age)
+                const rows = table.locator('tr')
+                await page.waitForTimeout(500)
+                for (let row of await rows.all()) {
+                    const value = await row.locator('td').last().textContent()
+                    if (age == '50') {
+                        expect(value).toEqual(' No data found ')
+                    } else {
+                        expect(value).toEqual(age)
+                    }
+                }
+            }
+        })
+    })
 })
